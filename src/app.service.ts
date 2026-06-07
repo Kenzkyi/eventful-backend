2bbde67b1cda7e59ab45dd8b5f3a3f1d0cb03229
Kenzkyi <ekeneokoye93@gmail.com>
2026-06-08 00:03:25 +0100

    fix: update greeting message in AppService to provide API documentation link

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to Eventful, the event planning app. Please visit /api/docs for API documentation.';
   }
 }
